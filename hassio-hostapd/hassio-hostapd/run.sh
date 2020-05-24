2ee8af1aeead8be80feacff70ba0b94bb789b572
nick2ld <14963497+nick2ld@users.noreply.github.com>
2020-05-25 02:43:45 +0300

    Update run.sh

--- hassio-hostapd/hassio-hostapd/run.sh
+++ hassio-hostapd/hassio-hostapd/run.sh
@@ -27,7 +27,7 @@ NETMASK=$(jq --raw-output ".netmask" $CONFIG_PATH)
 BROADCAST=$(jq --raw-output ".broadcast" $CONFIG_PATH)
 STADD="statics: "
 STADD+=$(jq --raw-output ".statics" $CONFIG_PATH)
-STADDKOL=$(echo "$STADD" | jq ".statics[] | length" $CONFIG_PATH)
+STADDKOL=$(echo "$STADD" | jq length ".statics[]" $CONFIG_PATH)
 echo "тест $STADDKOL"
 echo "тест $STADD"
 declare stmac
