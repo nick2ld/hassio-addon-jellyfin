3d1403fe56feafc9ffcd41ef15e65120d273b5f9
nick2ld <14963497+nick2ld@users.noreply.github.com>
2020-05-25 03:05:39 +0300

    Update run.sh

--- hassio-hostapd/hassio-hostapd/run.sh
+++ hassio-hostapd/hassio-hostapd/run.sh
@@ -25,7 +25,7 @@ CHANNEL=$(jq --raw-output ".channel" $CONFIG_PATH)
 ADDRESS=$(jq --raw-output ".address" $CONFIG_PATH)
 NETMASK=$(jq --raw-output ".netmask" $CONFIG_PATH)
 BROADCAST=$(jq --raw-output ".broadcast" $CONFIG_PATH)
-STADD=$(jq --raw-output ".statics" $CONFIG_PATH)
+STADD=$(jq --raw-output ".statics[]" $CONFIG_PATH)
 STADDKOL=$(echo "$STADD" | jq length ".[]" $STADD)
 echo "тест $STADDKOL"
 echo "тест $STADD"
